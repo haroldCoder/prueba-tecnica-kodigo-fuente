483b9aae4641db1a8a84ee2a48fa9c4330b41a02
haroldCoder <haroldc2005@gmail.com>
2026-04-10 17:45:52 -0500

    chore: migrate to Tailwind CSS v4 and implement custom color palette and navigation bar feature
    - feat: implement shadcnUi packge

---
+++ frontend/src/features/nav-bar/presentation/nav-bar.tsx
@@ -0,0 +1,17 @@
+export const NavBar = () => {
+    return (
+        <nav className="flex items-center justify-end px-6 py-4 bg-background border-b shadow-sm shadow-brandGray-900 w-full">
+            <div className="flex items-center">
+                <span className="text-xl font-bold tracking-tight  text-brandBlue-500">
+                    Kodigo Fuente
+                </span>
+            </div>
+
+            <div className="flex items-center space-x-4">
+
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
